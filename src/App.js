f0bee50487e4e264f61fc07c9eee8a077bf2891b
AbrorKh <43052046+AbrorKh@users.noreply.github.com>
2018-11-03 14:04:05 -0700

    f

--- src/App.js
+++ src/App.js
@@ -42,8 +42,8 @@ class App extends Component {
             <div className="col-6">
               <p className="text-light" id="txt1">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry/'/s standard dummy
-                text ever since the /1500s/, when an unknown printer took a galley
+                industry. Lorem Ipsum has been the industry//'//s standard dummy
+                text ever since the //1500s//, when an unknown printer took a galley
                 of type and scrambled it to make a type specimen book. It has
                 survived not only five centuries, but also the leap into
                 electronic typesetting, remaining essentially unchanged.
@@ -51,8 +51,8 @@ class App extends Component {
       
        <p className="text-light" id="txt1">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry/'/s standard dummy
-                text ever since the /1500s/, when an unknown printer took a galley
+                industry. Lorem Ipsum has been the industry//'//s standard dummy
+                text ever since the //1500s//, when an unknown printer took a galley
                 of type and scrambled it to make a type specimen book. It has
                 survived not only five centuries, but also the leap into
                 electronic typesetting, remaining essentially unchanged.
